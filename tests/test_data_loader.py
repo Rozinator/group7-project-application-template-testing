65c4df13676c2bc71d72c3934396dc645d96ee7e
Rozanne de Zoysa <rozannedezoysa@Rozannes-MacBook-Pro-9.local>
2025-11-17 21:43:10 -0500

    Initial commit with test json files and dataloader tester function

---
+++ tests/test_data_loader.py
@@ -0,0 +1,15 @@
+import json
+import os
+import tempfile
+import unittest
+
+import data_loader
+from model import Event, Issue
+
+
+class TestDataLoader(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
